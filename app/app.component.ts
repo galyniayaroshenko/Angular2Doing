04a1637380f61ccb3ad6b09ec92f5d85be5c14da
yaroshenko.g <yaroshenko270@gmail.com>
2016-07-27 16:30:16 +0300

    fix ways

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { TodoListComponent } from './components/todo-list/todo-list.component';
-import { Todo } from './components/shared/todo.model';
+import { Todo } from './shared/todo.model';
 import { TodoFormComponent } from './components/todo-form/todo-form.component';
 
 @Component({
