68f7891c7fcb2a5fe04beca3c993daf12cbfebb1
yaroshenko.g <yaroshenko270@gmail.com>
2016-07-27 13:02:05 +0300

    add task

--- app/app.component.ts
+++ app/app.component.ts
@@ -7,8 +7,17 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
     title: string;
+    todos: string[];
 
     constructor() {
         this.title = 'Angular 2Doing!';
+        this.todos = ['cdc'];
+    }
+    
+    addTodo(input:HTMLInputElement) {
+        let title = input.value;
+        input.value = '';
+        console.log('title', title);
+        this.todos.push(title);
     }
 }
